397f4eda1df2bbb6f6e3f2d7aa208713d3fdbd10
sorue <sorue@andreani.com>
2024-04-28 21:26:49 -0300

    fix: path bash script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
+set -x
 response=$(curl 'https://api.applivery.io/v1/integrations/builds' \
 -X POST \
 -H 'Authorization: bearer JiLk5I6WuID6gqNC1SdwLK_4' \
+-H 'Accept: application/json' \
 -F build=@'android/app/build/outputs/apk/release/app-release-unsigned.apk' \
 -F 'versionName=4.8' \
 -F 'changelog=' \
@@ -10,7 +12,7 @@ response=$(curl 'https://api.applivery.io/v1/integrations/builds' \
 2>&1)
 
 if [ $? -eq 0 ]; then
-    echo "API call succeeded. Response was: $response"
+    echo "API call succeeded. Response was: \"$response\""
 else
-    echo "API call failed. Error was: $response"
+    echo "API call failed. Error was: \"$response\""
 fi
